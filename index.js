29818d81a8a6030d87b4d98dfa0983b5abf1c655
Dave Gray <dgray@sparkpost.com>
2017-08-03 17:27:31 -0600

    need to call `context.succeed` with json, apparently

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 'use strict';
 //var AWS = require('aws-sdk');
 
-console.log('loading function');
 
 const bucketName = process.env.BUCKET_NAME;
-const createResponse = (statusCode, body) => {
-  return {
-    "statusCode": statusCode,
-    "body": body || ""
-  };
-};
 
 exports.store_batch = (event, context, callback) => {
   //var s3 = new AWS.S3();
-  console.log('would store batch to '+ bucketName);
-  return createResponse(200, "would store batch to "+ bucketName);
+  console.log("would store batch to "+ bucketName);
+  context.succeed({ statusCode: 200, body: "{\"foo\": \"would store batch to "+ bucketName +"\"}" });
+  return { statusCode: 200, body: "{\"foo\": \"would store batch to "+ bucketName +"\"}" };
 }
