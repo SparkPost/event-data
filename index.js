2a8794f8c7e6c15f49686d22d1b9072f0c67f678
Dave Gray <dgray@sparkpost.com>
2017-08-07 12:37:00 -0600

    store batches in s3, with key = batch id

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 'use strict';
-//var AWS = require('aws-sdk');
-
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 const bucketName = process.env.BUCKET_NAME;
 
 exports.store_batch = (event, context, callback) => {
-  //var s3 = new AWS.S3();
-  console.log("would store batch to "+ bucketName);
-  context.succeed({ statusCode: 200, body: "{\"foo\": \"would store batch to "+ bucketName +"\"}" });
-  return { statusCode: 200, body: "{\"foo\": \"would store batch to "+ bucketName +"\"}" };
+  var strEvent = JSON.stringify(event);
+  var batchId = null;
+  var err = null;
+  if (typeof event.headers === 'undefined') {
+    err = { error: "headers not found" };
+  } else {
+    batchId = event.headers['X-MessageSystems-Batch-ID'];
+    if (typeof batchId === 'undefined') {
+      err = { error: "batch id not found" };
+    }
+  }
+
+  if (err !== null) {
+    // to return client-visible messages, don't specify error
+    // log it out too so it hits cloudwatch
+    console.log(err.error);
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(err)
+    });
+    return;
+  }
+
+  var blen = event.body.length;
+  var params = { Bucket: bucketName, Key: batchId, Body: event.body };
+
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      callback(err);
+      return;
+    }
+
+    console.log(data);
+    callback(null, {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        message: "stored "+ blen +" bytes for batch "+ batchId
+      })
+    });
+  });
+
 }
